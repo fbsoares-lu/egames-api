8f58600387cd9ea2451cb657f9d7feea00774ca0
fbsoares-lu <lsoareshfb@gmail.com>
2023-05-23 15:11:11 -0300

    fix role routes

--- src/infra/http/routes/roles.routes.ts
+++ src/infra/http/routes/roles.routes.ts
@@ -3,11 +3,13 @@ import { Router, Request, Response } from "express";
 import { createRoleController } from "../../../modules/accounts/useCases/createRole";
 import { ResponseValidationBase } from "../validations/ResponseValidationBase";
 import { CreateRoleValidation } from "../validations/accounts/CreateRoleValidation";
+import { ensuredAuthentication } from "../middlewares/ensuredAuthentication";
 
 const rolesRoutes = Router();
 
 rolesRoutes.post(
   "/",
+  ensuredAuthentication,
   CreateRoleValidation.handle(),
   (request: Request, response: Response) => {
     ResponseValidationBase.handle(request, response);
