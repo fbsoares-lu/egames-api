f7df59c7b994540fb231d6942fe36b797a09bdbb
fbsoares-lu <lsoareshfb@gmail.com>
2023-06-03 13:25:17 -0300

    fix authentication file routes

--- src/infra/http/routes/files.routes.ts
+++ src/infra/http/routes/files.routes.ts
@@ -1,12 +1,18 @@
 import multer from "multer";
 import { Router } from "express";
 import { uploadFileController } from "../../../modules/files/useCases/uploadFile";
+import { ensuredAuthentication } from "../middlewares/ensuredAuthentication";
 
 const filesRoutes = Router();
 const upload = multer({ dest: "tmp", storage: multer.memoryStorage() });
 
-filesRoutes.post("/", upload.single("file"), (request, response) => {
-  return uploadFileController.handle(request, response);
-});
+filesRoutes.post(
+  "/",
+  ensuredAuthentication,
+  upload.single("file"),
+  (request, response) => {
+    return uploadFileController.handle(request, response);
+  }
+);
 
 export { filesRoutes };
